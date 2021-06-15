27d6a721147db1816c0692f3c1e22ab339079634
Judith Feltl <judith.feltl@live.at>
2021-06-15 22:46:21 +0200

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -35,7 +35,7 @@ class SignUp extends React.Component {
                 confirmPassword: ''
             });
         } catch (error) {
-            console.error(error);
+            console.log(error);
         }
     }
 
